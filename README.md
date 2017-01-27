b699990e6d949801e7cef4fb49fd1028cd11c4f3
Ryan Webb <dzanfox@gmail.com>
2017-01-27 07:43:26 -0600

    added mongo and passport functionality

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Demo using React with Authentication
 
 Part 1: https://vladimirponomarev.com/blog/authentication-in-react-apps-creating-components
+
 Part 2: https://vladimirponomarev.com/blog/authentication-in-react-apps-jwt
--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./config');
+
+//connect to DB and load models
+require('./server/models').connect(config.dbUri);
 
 const app = express();
 //look for static files
 app.use(express.static('./server/static/'));
 app.use(express.static('./client/dist/'));
+//parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended: false }));
+//pass the passport middleware
+app.use(passport.initialize());
+
+//passport strategies
+const localSignupStrategy = require('./server/passport/local-signup');
+const localLoginStrategy = require('./server/passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+//pass authentication checker middleware 
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
 
 //routes
 const authRoutes = require('./server/routes/auth');
